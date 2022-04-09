0d640b5ec96e48eff0e5c0710a12c17bfa178f84
missa <missajaziri@gmail.com>
2022-04-09 23:06:58 +0100

    unit-test-almost-done

--- angular-book-boutique-testing/src/app/services/book-handler.service.spec.ts
+++ angular-book-boutique-testing/src/app/services/book-handler.service.spec.ts
@@ -1,3 +1,4 @@
+import { HttpClientModule } from '@angular/common/http';
 import { TestBed } from '@angular/core/testing';
 
 import { BookHandlerService } from './book-handler.service';
@@ -6,7 +7,7 @@ describe('BookHandlerService', () => {
   let service: BookHandlerService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({ imports: [HttpClientModule] });
     service = TestBed.inject(BookHandlerService);
   });
 
